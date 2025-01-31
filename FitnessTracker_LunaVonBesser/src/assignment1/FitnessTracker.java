dd9284bd928432f46bbfbe7b1d982e58a697a44a
Luna Vee <lunavee.b@gmail.com>
2025-01-31 14:57:16 -0500

    committing again after save.

--- FitnessTracker_LunaVonBesser/src/assignment1/FitnessTracker.java
+++ FitnessTracker_LunaVonBesser/src/assignment1/FitnessTracker.java
@@ -9,12 +9,14 @@ public class FitnessTracker {
     // TODO 1: INCLUDE YOUR ATTRIBUTES HERE. CHECK THE UML CLASS DIAGRAM TO INCLUDE ALL ATTRIBUTES LISTED AND THEIR TYPES.
 
     // START CODE
+	
 	private String firstName;
 	private String lastName;
 	private String gender;
 	private int stepsTaken;
 	private int caloriesBurned;
 	private int activeMinutes;
+	
     // END CODE
 
     // TODO 2: INCLUDE YOUR CONSTRUCTOR HERE.
@@ -25,8 +27,7 @@ public class FitnessTracker {
     // TODO 3: INCLUDE YOUR SET AND GET METHODS HERE. CHECK THE UML CLASS DIAGRAM TO ENSURE ALL ARE COMPLETE.
 
     // START CODE
-    // END CODE
-
+    
     public String getFirstName() {
 		return firstName;
 	}
@@ -34,6 +35,48 @@ public class FitnessTracker {
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public int getStepsTaken() {
+		return stepsTaken;
+	}
+
+	public void setStepsTaken(int stepsTaken) {
+		this.stepsTaken = stepsTaken;
+	}
+
+	public int getCaloriesBurned() {
+		return caloriesBurned;
+	}
+
+	public void setCaloriesBurned(int caloriesBurned) {
+		this.caloriesBurned = caloriesBurned;
+	}
+
+	public int getActiveMinutes() {
+		return activeMinutes;
+	}
+
+	public void setActiveMinutes(int activeMinutes) {
+		this.activeMinutes = activeMinutes;
+	}
+	
+	// END CODE
 
 	// TODO 4: THIS METHOD SHOULD DISPLAY ALL USER FITNESS DATA. DO NOT MISS ANY DATA/INFORMATION.
     public void displayFitnessData() {
